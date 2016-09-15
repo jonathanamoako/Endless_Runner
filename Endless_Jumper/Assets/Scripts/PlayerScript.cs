5ee4d41b347e8d36b69a626973fe424b58408ee2
jonathanamoako <j.amoako@outlook.com>
2016-09-15 12:33:36 +0200

    Progress
    Added player movement

---
+++ Endless_Jumper/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed;
+
+    private Vector3 dir;
+
+	// Use this for initialization
+	void Start ()
+    {
+        dir = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	if (Input.GetMouseButtonDown(0))
+        {
+            if (dir == Vector3.forward)
+            {
+                dir = Vector3.left;
+            }
+            else
+            {
+                dir = Vector3.forward;
+            }
+        }
+
+        float amountToMove = speed * Time.deltaTime;
+
+        transform.Translate(dir * amountToMove);
+	}
+}
---
+++ Endless_Jumper/Assets/Scripts/TileManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+    public GameObject leftTilePrefab;
+    public GameObject currentTile;
+
+	// Use this for initialization
+	void Start ()
+    {
+	for (int i = 0; i < 10; i++)
+        {
+            SpawnTiles();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SpawnTiles ()
+    {
+        Instantiate(leftTilePrefab, currentTile.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
+    }
+}
