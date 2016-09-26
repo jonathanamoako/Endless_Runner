2843af6eb86848f0aacaf717296be9555eb33f59
JeffrinDumas <jeffrin.e.d@live.nl>
2016-09-26 11:33:07 +0200

    commitingUI

---
+++ Arduino/Assets/Arduino.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+
+
+public class Arduino : MonoBehaviour {
+   
+
+	// Use this for initialization
+	void Start () {
+        stream = new SerialPort("COM4", 9600);
+        stream.ReadTimeout = 50;
+        stream.Open();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Endless_Jumper/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+	}
+	
+	    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+        public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
