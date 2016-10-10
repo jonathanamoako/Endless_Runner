f7229ec75b0aa900434f9ab76016ad4809c16f66
JeffrinDumas <jeffrin.e.d@live.nl>
2016-10-10 09:35:55 +0200

    Click to start

--- Endless_Jumper/Assets/Scripts/MySQL.cs
+++ Endless_Jumper/Assets/Scripts/MySQL.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using System.Collections;
 
 public class MySQL : MonoBehaviour
@@ -37,4 +37,4 @@ public class MySQL : MonoBehaviour
         }
 
     }
-}
+}*/
---
+++ Endless_Jumper/Assets/Scripts/TextTimer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float time = 2f; //Seconds to read the text
+
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+
+}
+
