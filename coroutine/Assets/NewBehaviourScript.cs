8681cba0c48a04a00ae8bfe2cc487e3f6d90eccd
JeffrinDumas <jeffrin.e.d@live.nl>
2016-10-12 09:58:46 +0200

    menu

---
+++ coroutine/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private IEnumerator hello;
+
+    void Start()
+    {
+      
+        print("Srating " + Time.time);
+        hello = WaitForPrint(2.0f);
+        StartCoroutine(hello);
+
+        print("Before WaitAndPrint Finishes " + Time.time);
+      }
+
+    private IEnumerator WaitForPrint(float waitTime)
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            print("WaitAndPrint " + Time.time);
+        }
+   
+    }
+}
---
+++ coroutine/Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+    [SerializeField]
+    private float timerTime;
+
+    void Start()
+    {
+        StartCoroutine(RunTime());
+    }
+
+	IEnumerator RunTime()
+    {
+        float timer = 0;
+            
+            while ( timer <timerTime)
+        {
+            timer += Time.deltaTime;
+            print(timer);
+            yield return null;
+        }
+        print("Timer Klaar");
+    }
+}
